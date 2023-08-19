a0f50241009741cc3ebc7427d9eb766938f229a3
Lina Marcela Landys Samboni <lina_landys@MacBook-Pro-de-Lina.local>
2023-08-18 19:03:04 -0500

    aaa

---
+++ index.js
@@ -0,0 +1,19 @@
+const calificaciones = {
+  A: 90 < 100,
+  B: 80 < 89,
+  C: 70 < 79,
+  D: 60 < 69,
+  F: 0 > 60,
+};
+console.log(calificaciones);
+
+
+function passed(calificar) {
+  if (true) {
+    for (let i = 0; i < 60; i++) {
+      const calificar = calificaciones[i < 69];
+      return passed;
+    }
+  }
+}
+console.log(passed);
